39e77ffc5df74292cac7bd9ba3d11e68d3e1784a
MundM2007 <schaefer781@gmx.de>
2025-04-27 20:14:41 +0200

    wipe of all files to upload new ones

--- kubejs/startup_scripts/unification/slurryinitializejavaclasses.js
+++
@@ -1,5 +0,0 @@
-// priority: 110
-if(Platform.isLoaded('mekanism')) {
-    let $SlurryDeferredRegister = java('mekanism.common.registration.impl.SlurryDeferredRegister')
-    global.SLURRY = new $SlurryDeferredRegister('unification')
-}
